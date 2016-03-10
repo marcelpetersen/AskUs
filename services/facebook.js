3f7ca213fabe64bb11df781932ef00c8f849c637
Renan Deswarte <renandeswarte@gmail.com>
2016-03-09 16:51:35 -0800

    feat: use promises for FB api return

--- services/facebook.js
+++ services/facebook.js
@@ -1,8 +1,22 @@
 angular.module('myApp.facebookService', [])
 
-.factory('facebook', function() {
+.factory('facebook', function($q, $rootScope) {
+
+ resolve = function(errval, retval, deferred) {
+    $rootScope.$apply(function() {
+      if (errval) {
+        deferred.reject(errval);
+      } else {
+        retval.connected = true;
+        deferred.resolve(retval);
+      }
+    });
+  }
   return {
+
+
     getFriends: function($scope, userDetail) {
+        var deferred = $q.defer();
         if (userDetail) {
           userDetail = JSON.parse(userDetail);
           // FACEBOOK GET FRIENDS LIST
@@ -10,19 +24,45 @@ angular.module('myApp.facebookService', [])
             "/" + userDetail.facebook.id + "/friends?access_token=" + userDetail.facebook.accessToken + "&fields=name,id,email,picture",
             function (response) {
               if (response && !response.error) {
-                $scope.friendsList = response.data;
-                $scope.$apply();                
-                $scope.$broadcast('scroll.refreshComplete');
+                resolve(null, response.data, deferred);
               } else {
-                console.log('error', response.error)
-                $scope.$broadcast('scroll.refreshComplete');
+                resolve(response.error, null, deferred);
               }
             }
           );
         } else {
-          $scope.noFriend = true;
-          $scope.$broadcast('scroll.refreshComplete');
+          resolve(null, "no friend", deferred);
+          // $scope.noFriend = true;
+          // $scope.$broadcast('scroll.refreshComplete');
         }
+         promise = deferred.promise;
+    
+      return promise;
     }
   }
+
+  // return {
+    // getFriends: function($scope, userDetail) {
+    //     if (userDetail) {
+    //       userDetail = JSON.parse(userDetail);
+    //       // FACEBOOK GET FRIENDS LIST
+    //       FB.api(
+    //         "/" + userDetail.facebook.id + "/friends?access_token=" + userDetail.facebook.accessToken + "&fields=name,id,email,picture",
+    //         function (response) {
+    //           if (response && !response.error) {
+    //             $scope.friendsList = response.data;
+    //             $scope.$apply();                
+    //             $scope.$broadcast('scroll.refreshComplete');
+    //           } else {
+    //             console.log('error', response.error)
+    //             $scope.$broadcast('scroll.refreshComplete');
+    //           }
+    //         }
+    //       );
+    //     } else {
+    //       $scope.noFriend = true;
+    //       $scope.$broadcast('scroll.refreshComplete');
+    //     }
+    // }
+  // }
 });
--- views/friends/friends.js
+++ views/friends/friends.js
@@ -7,19 +7,25 @@ angular.module('myApp.friends', ['myApp.env'])
 
   var userInfo = $localstorage.get('firebase:session::ionic-fboauth');
 
-  facebook.getFriends($scope, userInfo);
+  // console.log(facebook.getFriends($scope, userInfo));
+  facebook.getFriends($scope, userInfo).then(function(data) {
+    $scope.friendsList = data;
+  })
 
   $scope.doRefresh = function() {
-    facebook.getFriends($scope, userInfo);
-    angular.element('.text-pulling').addClass('hidden');
-    angular.element('.icon-pulling').addClass('hidden');
+    facebook.getFriends($scope, userInfo).then(function(data) {
+    $scope.friendsList = data;
+    $scope.$broadcast('scroll.refreshComplete');
+  });
+    // angular.element('.text-pulling').addClass('hidden');
+    // angular.element('.icon-pulling').addClass('hidden');
     // $timeout(function(){
     //   $scope.$broadcast('scroll.refreshComplete');
     // }, 400);    
   };
 
-  $scope.$on('scroll.refreshComplete', function() {
-      angular.element('.text-pulling').removeClass('hidden');
-      angular.element('.icon-pulling').removeClass('hidden');
-  });
+  // $scope.$on('scroll.refreshComplete', function() {
+  //     angular.element('.text-pulling').removeClass('hidden');
+  //     angular.element('.icon-pulling').removeClass('hidden');
+  // });
 });
