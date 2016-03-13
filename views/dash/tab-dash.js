85a54741c8977c8b787e9badc818aa30f59ad5b2
Renan Deswarte <renandeswarte@gmail.com>
2016-03-13 01:44:56 -0800

    feat: add zoom to modal, extra animation modal

--- views/dash/tab-dash.js
+++ views/dash/tab-dash.js
@@ -1,11 +1,11 @@
 angular.module('myApp.dashTab', ['myApp.env'])
 
-.controller('DashCtrl', function($scope, Post, $timeout, $rootScope, $ionicModal, $ionicSlideBoxDelegate) {
-
+.controller('DashCtrl', function($ionicPlatform, $scope, Post, $timeout, $rootScope, $ionicModal, $ionicSlideBoxDelegate) {
+  
   $scope.posts;
   $scope.aImages;
 
-  console.log('Dash Ctrl First call');
+  // console.log('Dash Ctrl First call');
 
   $rootScope.$on('dashRefresh', function() {
     $scope.doRefresh();
@@ -40,7 +40,9 @@ angular.module('myApp.dashTab', ['myApp.env'])
 
   $ionicModal.fromTemplateUrl('image-modal.html', {
     scope: $scope,
-    animation: 'slide-in-up'
+    // animation: 'slide-in-up'
+    animation: 'mh-slide'
+
   }).then(function(modal) {
     $scope.modal = modal;
   });
