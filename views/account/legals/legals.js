b64d2e6b0255f45e2c6670263500a25ea0fbb58f
Renan Deswarte <renandeswarte@gmail.com>
2016-04-06 14:16:08 -0700

    add privacy content

--- views/account/legals/legals.js
+++ views/account/legals/legals.js
@@ -2,4 +2,21 @@ angular.module('myApp.legals', ['myApp.env'])
 
 .controller('legalsCtrl', ['$scope', function($scope) {
 
+  var options = {
+    location: 'yes',
+    clearcache: 'yes',
+    toolbar: 'no'
+  };
+
+  $scope.openBrowser = function(url) {
+    cordova.InAppBrowser.open(url, '_blank', options)
+      .then(function(event) {
+        console.log("external link opened");
+      })
+      .catch(function(event) {
+        console.log("external link error");
+      });
+  }
+
+
 }]);
