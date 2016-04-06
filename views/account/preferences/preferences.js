0899afbd6da58cf10a4ec45a9e845ddf5caf0665
Renan Deswarte <renandeswarte@gmail.com>
2016-04-06 11:07:01 -0700

    feat: suspend my account WIP

--- views/account/preferences/preferences.js
+++ views/account/preferences/preferences.js
@@ -1,18 +1,30 @@
 angular.module('myApp.preferences', ['myApp.env'])
 
 .controller('preferencesCtrl', ['$scope', 'userAuth', 'currentUserInfos', function($scope, userAuth, currentUserInfos) {
-  $scope.settings = {
-    enableNotifications: true
-  };
 
+  var pageName = "#preferences-page";
+  angular.element(pageName + " .loading").hide();
   var userInfos = currentUserInfos.currentUserInfoGet();
 
   $scope.suspendAccountFacebook = function() {
+    angular.element(pageName + " .loading").show();
+    angular.element(pageName +' .loading-icon').addClass('spin');
+    angular.element(pageName + ' .button.suspend').prop("disabled",true);
     userAuth.suspendAccountFacebook(userInfos.id).then(function(response) {
-      console.log(response);
+      // console.log(response);
+      angular.element(pageName + " .loading").hide();
+      angular.element(pageName +' .loading-icon').removeClass('spin');
+      angular.element(pageName + ' .button.suspend').prop("disabled",false);
     }, function(error) {
       console.log(error);
-      $scope.openErrorModal();
+      angular.element(pageName + " .loading").hide();
+      angular.element(pageName +' .loading-icon').removeClass('spin');
+      angular.element(pageName + ' .button.suspend').prop("disabled",false);
+      if (error.data.error === "no such user") {
+        userAuth.logoutFacebook();
+      } else {
+        $scope.openErrorModal();
+      }
     });
   };
 
